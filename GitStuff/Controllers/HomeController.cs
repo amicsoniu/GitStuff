0fed7de1e6b6cfeb64e583d7d4c8aba6fb51d160
Michael V <mvataga@my.bcit.ca>
2019-10-03 10:07:36 -0700

    Added Michael

--- GitStuff/Controllers/HomeController.cs
+++ GitStuff/Controllers/HomeController.cs
@@ -39,6 +39,11 @@ namespace GitStuff.Controllers
             return View();
         }
 
+        public IActionResult Michael()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
