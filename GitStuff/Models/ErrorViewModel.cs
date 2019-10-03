898540691fb5e0289de71889f943dde3bad7f3e6
amicsoniu <amicsoniu@gmail.com>
2019-10-03 09:25:05 -0700

    Add project files.

---
+++ GitStuff/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GitStuff.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
